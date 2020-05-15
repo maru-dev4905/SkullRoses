8a2d228b0edc59d5513372484eead6acd29653e8
Seungyeol <marudev4905@gmail.com>
2020-05-15 21:04:52 +0900

    hambuger menu show/hide function 추가

--- src/js/hambuger.js
+++ src/js/hambuger.js
@@ -1,29 +1,49 @@
 const HambugerBtn = document.querySelector(".side--btn");
 const nav = document.querySelector(".side");
 
+const MOBILE = "719";
+
 let navItem = document.querySelectorAll(".side--nav--item");
 
 let hambugerCheck = "close";
+let windowWidth;
+let windowHeight;
 
+// Hambuger menu show / hide function 
+window.addEventListener("resize", () => {
+    windowWidth = window.innerWidth;
+    windowHeight = window.innerHeight;
+    console.log(windowWidth, windowHeight);
+
+    if (windowWidth <= MOBILE) {
+        HambugerBtn.style.opacity = "1";
+        HambugerBtn.style.zIndex = "5500";
+    } else {
+        HambugerBtn.style.opacity = "0";
+        HambugerBtn.style.zIndex = "-5500";
+    }
+});
+
+// Hambuger menu click event
 HambugerBtn.addEventListener("click", () => {
     if (hambugerCheck == "close") {
         nav.style.zIndex = "5000";
         nav.style.opacity = "1";
         hambugerCheck = "open"
-        for(let i = 0; i < navItem.length; i++){
+        for (let i = 0; i < navItem.length; i++) {
             navItem[i].style.animationName = "fadeUp";
             navItem[i].style.animationDuration = "1s";
-            navItem[i].style.animationDelay =  "0."+i+"s";
+            navItem[i].style.animationDelay = "0." + i + "s";
         }
+        
     } else {
         nav.style.zIndex = "-5000";
         nav.style.opacity = "0";
         hambugerCheck = "close"
-        for(let i = 0; i < navItem.length; i++){
-            navItem[i].style.animationName="";
+        for (let i = 0; i < navItem.length; i++) {
+            navItem[i].style.animationName = "";
             navItem[i].style.animationDuration = "";
-            navItem[i].style.animationDelay="";
-
+            navItem[i].style.animationDelay = "";
         }
     }
 });
